b547ee11c57e62cbd66adb4b508bb60329e44853
jflaga <flaga.jeremiah@gmail.com>
2026-08-20 21:54:08 +0800

    Practice Problems for CSGDSAP-Vol1: Leetcode 217 Contains Duplicate

---
+++ chron/2026-07-29 Common-Sense DSA Practice/02 Ch 4–7 Big O & Optimization/02.03 Leetcode 217 Contains Duplicate.py
@@ -0,0 +1,61 @@
+#####################
+# Leetcode 217 Contains Duplicate
+# https://leetcode.com/problems/missing-number/description/
+
+# Aug 20, 2026
+# Start: 9:44 PM
+
+# Initial solution: 
+#   Use hash table / dictionary
+#   
+
+# End: 9:53 PM
+# Submission Link: https://leetcode.com/problems/contains-duplicate/submissions/2113994620/
+#####################
+
+
+from typing import List
+import unittest
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        num_exists_dict = {}
+
+        for i in nums:
+            if num_exists_dict.get(i) == True:
+                return True
+            
+            num_exists_dict[i] = True
+
+        return False
+        
+
+        
+
+#####################
+
+
+class Tests(unittest.TestCase):
+  
+    def __init__(self, *args, **kwargs):
+        unittest.TestCase.__init__(self, *args, **kwargs) 
+
+    def test_1(self):
+        soln = Solution()
+        result = soln.containsDuplicate([1,2,3,1])
+        self.assertEqual(True, result)
+
+    def test_2(self):
+        soln = Solution()
+        result = soln.containsDuplicate([1,2,3,4])
+        self.assertEqual(False, result)
+
+    def test_3(self):
+        soln = Solution()
+        result = soln.containsDuplicate([1,1,1,3,3,4,3,2,4,2])
+        self.assertEqual(True, result)
+
+
+
+if __name__ == '__main__':
+  unittest.main()
